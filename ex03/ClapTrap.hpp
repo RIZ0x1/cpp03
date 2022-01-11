e0e4993ba812f1348d3c6609a1498f30e1f3a02c
RIZ1 <62781747+RIZ0x1@users.noreply.github.com>
2022-01-11 13:50:47 +0300

    Colors alignment fixed :)

--- ex03/ClapTrap.hpp
+++ ex03/ClapTrap.hpp
@@ -3,12 +3,12 @@
 #include <string>
 #include <iostream>
 
-#define NORMAL "\033[0m"
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define MAGENTA "\033[35m"
+#define NORMAL	"\033[0m"
+#define ORANGE	"\033[31m"
+#define GREEN	"\033[32m"
+#define YELLOW	"\033[33m"
+#define BLUE	"\033[34m"
+#define MAGENTA	"\033[35m"
 
 class ClapTrap
 {
